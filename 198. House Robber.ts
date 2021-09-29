f0fc892e8121787426cae31b4b406049f38322cf
Xynthrax <duluu_666@yahoo.com>
2021-09-29 00:29:57 -0500

    Create 198. House Robber.ts

---
+++ 198. House Robber.ts
@@ -0,0 +1,23 @@
+function rob(nums: number[]): number {
+    
+    if(nums.length == 1) return nums[0];
+    
+    if(nums.length == 2) return Math.max(nums[0], nums[1]);
+    
+    let dp = [];
+    
+    dp[0] = nums[0];
+    dp[1] = nums[1];
+    
+    for( let i = 2; i < nums.length; i++){
+        dp[i] = nums[i];
+        let max = 0;
+        for( let j = 0; j < i-1; j++){
+            max = Math.max( max, dp[j] );
+        }
+        dp[i]+= max;
+    }
+    // console.log(dp);
+    return Math.max(dp[dp.length-1], dp[dp.length-2]);
+
+};
